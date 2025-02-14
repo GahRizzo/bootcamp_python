2d302593a2e899d3729fcbe7de9b7728bd1f3355
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-14 07:35:59 -0300

    Deleting duplicity

--- aula_1/exemplo_00.py
+++
@@ -1,3 +0,0 @@
-# Crie um programa que o usuário digite o seu nome e retorna o número de caracteres
-nome = input("Digite o seu nome:")
-print(f"Seu nome tem {len(nome)} caracteres")
--- aula_1/exemplo_01.py
+++
@@ -1,5 +0,0 @@
-## Criar um programa onde o usuário digite dois valores e apareça a soma
-valor_1 = int(input("Digite o primeiro valor inteiro:"))
-valor_2 = int(input("Digite o segundo valor inteiro:"))
-
-print(f"A soma dos valores é: {valor_1 + valor_2}")
