6d8a2e2f9405a45e2c99ae334578561d2fc33c3b
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-14 09:18:27 -0300

    Adding exercise 06

---
+++ aula_02/exercicio_06.py
@@ -0,0 +1,8 @@
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+first_value = float(input('Type the first value: '))
+second_value = float(input('Type the second value: '))
+
+sum = first_value + second_value
+
+print(f'The sum of values is: {sum:0.1f}')
