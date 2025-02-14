b7769d9bcc19f6dc2bb545b8d0f245bdea47c18d
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-14 08:58:35 -0300

    Adding exercise_03

---
+++ aula_02/exercicio_03.py
@@ -0,0 +1,8 @@
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+first_number = int(input('Type the first number: '))
+second_number = int(input('Type the second number: '))
+
+operation = first_number * second_number
+
+print(f'The first number times the second number is {operation}')
