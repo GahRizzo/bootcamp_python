e47f366211421fa21b767700e66129514ef22209
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-14 19:00:35 -0300

    Uploading exercise 15

---
+++ aula_02/exercicio_15.py
@@ -0,0 +1,8 @@
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+first_string = input("Type the first word: ")
+second_string = input("Type the second word: ")
+
+string_union = first_string + second_string
+
+print(f'The union between the words is: {string_union}')
