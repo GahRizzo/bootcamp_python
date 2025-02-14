6e1ff0d9499ddf5ea02507468a99857c5d67dfec
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-14 18:44:38 -0300

    Uploading exercise 12

---
+++ aula_02/exercicio_12.py
@@ -0,0 +1,7 @@
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+username = input('Type your name: ')
+
+lower_username = username.lower()
+
+print(f'Your user name lower is: {lower_username}')
