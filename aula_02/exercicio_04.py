2fe465257adb060d3ab9b0bf6403521ba819f6e7
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-14 09:04:24 -0300

    Adding exercise 04

---
+++ aula_02/exercicio_04.py
@@ -0,0 +1,8 @@
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+first_number = int(input('Type the first number: '))
+second_number = int(input('Type the second number: '))
+
+division = first_number // second_number
+
+print(f'The itenger division between the first and second number is {division}')
