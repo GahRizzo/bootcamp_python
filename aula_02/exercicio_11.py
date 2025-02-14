1fe3bfd80feae092f53d058f5e2fc6fadea4acc4
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-14 12:12:11 -0300

    Uploading exercise 11

---
+++ aula_02/exercicio_11.py
@@ -0,0 +1,7 @@
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+word_input = input('Type a word: ')
+
+upper_word = word_input.upper()
+
+print(f'The upper word is {upper_word}')
