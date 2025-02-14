57744f573b1feac6ae554ba4fa87c4dcdbbd411c
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-14 09:14:41 -0300

    Adding exercise 05

---
+++ aula_02/exercicio_05.py
@@ -0,0 +1,8 @@
+#5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+num_input = int(input("Type a number: "))
+EXPOENT = 2
+
+number_square = num_input ** 2
+
+print(f'The square of typed number is: {number_square}')
