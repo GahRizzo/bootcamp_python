9659dcabf4c95c3d7b7a5185c8255f4daf57bad9
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-14 08:41:30 -0300

    Adding exercise 2

---
+++ aula_02/exercicio_02.py
@@ -0,0 +1,8 @@
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+num_input = float(input("Type a number: "))
+DIVISOR = 5
+
+rest = num_input % 5
+
+print(f"The rest of the division is: {rest:.0f}")
