6de459d1d852865e8dd3e311424e282aecc98d6e
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-14 08:52:00 -0300

    Deleting exercise_1

--- aula_02/exercicios_1.py
+++
@@ -1,9 +0,0 @@
-# #### Inteiros (`int`)
-# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-
-first_value = int(input('Type the first value: '))
-second_value = int(input('Type the second value: '))
-
-sum = first_value + second_value
-
-print(f'The sum of values is: {sum}')
