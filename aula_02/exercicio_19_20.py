1ad695ba789c956c6bce87e599f290b0e1d9568d
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-15 07:17:34 -0300

    Adding description in exercise

--- aula_02/exercicio_19_20.py
+++ aula_02/exercicio_19_20.py
@@ -1,4 +1,5 @@
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
 
 first_number = float(input("Type the first number: "))
 second_number = float(input("Type the second number: "))
