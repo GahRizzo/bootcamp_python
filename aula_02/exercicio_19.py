785cf7463e7c2b7753a477bea4211a68aaa2bfa7
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-15 07:15:14 -0300

    Uploading exercise 19

---
+++ aula_02/exercicio_19.py
@@ -0,0 +1,9 @@
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+first_number = float(input("Type the first number: "))
+second_number = float(input("Type the second number: "))
+
+if first_number == second_number:
+    print(f'The first and second number are equal')
+else:
+    print(f"""The first and second number doesn't equal""")
