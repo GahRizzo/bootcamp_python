afc9be6acf4b87711b2befaa88f49fb58805c0db
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-02-23 19:55:40 -0300

    Adjusting code, exercise 12

--- aula_03/exercicio_12.py
+++ aula_03/exercicio_12.py
@@ -1,12 +1,14 @@
 ### Exercício 12. Validação de Entrada
 # Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
 
-value = 999
-
-while value < 0 or value > 10:
+while True:
     try:
         value = int(input('Type a number between 1 and 10: '))
+        if value < 0 or value > 10:
+            print('Invalid Value')
+            pass
+        else:
+            print('✔️  Sucess - your number is validated')
+            break
     except ValueError or TypeError as e:
         print(f'{e} - You need input a integer number')
-print('✔️  Sucess - your number is validated')
-
