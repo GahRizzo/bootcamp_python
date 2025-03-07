a16bcac14aec0827c5eba23f996f79266a64ca51
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-03-07 08:22:31 -0300

    Exercise 2

---
+++ aula_04/exercicio_02.py
@@ -0,0 +1,8 @@
+# Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+programming_list = ["Python", "Java", "C++", "JavaScript"]
+
+programming_list.remove("C++")
+programming_list.append("Ruby")
+
+print(f'The programming list is: {programming_list}')
