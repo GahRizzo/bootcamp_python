057c307944568e5581ddd7e0b104cf72690f6ee5
Gabriel Rizzo da Silva <gabriel.rizzo1994@gmail.com>
2025-03-13 08:33:30 -0300

    Adding exercise 09

---
+++ aula_04/exercicio_09.py
@@ -0,0 +1,7 @@
+## Objetivo: Dado um conjunto de números, calcular a média.
+
+numbers = [12,14,21,55,23,63,98,75]
+
+avg = sum(numbers)/len(numbers)
+
+print(f'The average value of a list is: {avg:0.2f}')
